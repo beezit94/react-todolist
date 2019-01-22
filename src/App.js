d40e7cd4c9f7f3b9545e7231f51cf8332843ea71
Birinchi <Bijitashya.Birinchi@emc.com>
2019-01-22 17:38:59 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import Addtodo from './components/Addtodo';
+import About from './components/pages/About';
+import uuid from 'uuid';
+import axios from 'axios';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
+  state = {
+    todos: [
+      // {
+      //   id: uuid.v4(),
+      //   title: 'Shoppping',
+      //   completed: false
+      // },
+      // {
+      //   id: uuid.v4(),
+      //   title: 'Meeting',
+      //   completed: false
+      // },
+      // {
+      //   id: uuid.v4(),
+      //   title: 'phone call',
+      //   completed: false
+      // }
+    ]
+  };
+
+  componentDidMount() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => this.setState({ todos: res.data }));
+  }
+
+  addTodo = title => {
+    // const newTodo = {
+    //   id: uuid.v4(),
+    //   title,
+    //   completed: false
+    // };
+    // this.setState({ todos: [...this.state.todos, newTodo] });
+    axios
+      .post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed: false
+      })
+      .then(res => this.setState({ todos: [...this.state.todos, res.data] }));
+  };
+
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  delTodo = id => {
+    // this.setState({
+    //   todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    // });
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res =>
+        this.setState({
+          todos: [...this.state.todos.filter(todo => todo.id !== id)]
+        })
+      );
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <React.Fragment>
+                  <Addtodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
